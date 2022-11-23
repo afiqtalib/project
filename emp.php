fccbbc0589b833b39ca8429ff6048ed4be963bfd
Muhammad Afiq <afiqtalib2580@gmail.com>
2022-11-23 15:14:15 +0800

    KOMIT

--- emp.php
+++ emp.php
@@ -152,10 +152,22 @@
             <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script> -->
             
             <!-- Page filter table -->
-            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-            <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-            <script src="https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js"></script>
+            <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script> -->
+            <!-- <script src="https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js"></script> -->
 
+            <!-- LINK DATA TABLE EXPORT baru -->
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"> 
+            <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
+
+            <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+            <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
             <script>
                 $(document).ready(function () {
                     // Setup - add a text input to each footer cell
@@ -228,7 +240,8 @@
 
 </html>
 
-<style>
+<!-- css wasap.my -->
+<!-- <style>
                 
     /* text/css bootstrap.css ( ASCII text, with very long lines, with CRLF line terminators ) */
     /*!
@@ -6477,7 +6490,7 @@
         margin-top: 0.5em;
     }
     }
-</style>
+</style> -->
 
 <?php  
         include 'includes/footer.php';
